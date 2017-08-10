eb4ecc24de3f22e51cc5b393ca801d32db4ac57c
Higor Vieira <30779238+ighro@users.noreply.github.com>
2017-08-09 23:04:15 -0300

    Add files via upload

---
+++ olaMundo.java
@@ -0,0 +1,22 @@
+// Exemplo de código comentado
+/*
+
+* @Author: Higor Vasconcelos	
+* @date: 06 de ago de 2017	
+* @Time: 15:50
+*/
+
+public class olaMundo{
+	
+	public static void main(String[] args){
+		
+		System.out.println("Ola Mundo!");
+		String nome = "higor";
+		int nascimento = 1995;
+		double idade = 22.4;
+		
+		System.out.println("Meu nome eh: " + nome + " minha idade eh: " + idade + " Nasci em: " + nascimento);
+		
+	}
+	
+}
